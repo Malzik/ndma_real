aa7e506882214358598fb0f8a03a6ef69d577003
SepraDC <lucas.birac@epsi.fr>
2022-04-06 09:51:44 +0200

    Ajout zone de détéction

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+/* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
+  ViroBox,
   ViroARSceneNavigator,
   ViroAmbientLight,
   Viro3DObject,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
@@ -13,49 +16,57 @@ const HelloWorldSceneAR = () => {
 
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
-
   }
 
+  ViroARTrackingTargets.createTargets({
+    'targetOne': {
+      source: require('./res/rhino.png'),
+      orientation: 'Up',
+      physicalWidth: 0.5, // real world width in meters
+    },
+  });
+
   return (
-      <ViroARScene onTrackingUpdated={this.onInitialized}>
-        <ViroAmbientLight color="#FFFFFF" />
-        <Viro3DObject source={require('./res/12140_Skull_v3_L2.obj')}
-            resources={[
-                require('./res/12140_Skull_v3_L2.mtl'),
-                require('./res/Skull.jpg'),
-            ]}
-            highAccuracyEvents={true}
-            position={[0, 2, -3]}
-            scale={[0.05, 0.05, 0.05]}
-            rotation={[-45, 0, 0]}
-            type="OBJ"
-            //transformBehaviors={["billboard"]}
-        />
-      </ViroARScene>
-  );
-};
+    <ViroARScene onTrackingUpdated={this.onInitialized}>
+      <ViroARImageMarker target={'targetOne'} onAnchorFound={()=> console.log('Trouvé !!')}>
+      <ViroAmbientLight color="#FFFFFF" />
+      <Viro3DObject
+        source={require('./res/12140_Skull_v3_L2.obj')}
+        resources={[
+          require('./res/12140_Skull_v3_L2.mtl'),
+          require('./res/Skull.jpg'),
+        ]}
+        highAccuracyEvents={true}
+        position={[0, 2, -3]}
+        scale={[0.05, 0.05, 0.05]}
+        rotation={[-45, 0, 0]}
+        type="OBJ"
+        //transformBehaviors={["billboard"]}
+      />
+      </ViroARImageMarker>
+    </ViroARScene>
+    );
+  };
 
-
-
-export default () => {
-  return (
-    <ViroARSceneNavigator
+  export default () => {
+    return (
+      <ViroARSceneNavigator
       autofocus={true}
       initialScene={{
         scene: HelloWorldSceneAR,
       }}
       style={styles.f1}
-    />
-  );
-};
+      />
+      );
+    };
 
-var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
+    var styles = StyleSheet.create({
+      f1: {flex: 1},
+      helloWorldTextStyle: {
+        fontFamily: 'Arial',
+        fontSize: 30,
+        color: '#ffffff',
+        textAlignVertical: 'center',
+        textAlign: 'center',
+      },
+    });
